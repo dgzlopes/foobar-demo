5492ce1aa667fc2fbaa547184c4b73af27ac726b
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2021-03-04 11:39:06 +0100

    From OpenTracing to OpenTelemetry
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- bar.py
+++ bar.py
@@ -1,45 +1,31 @@
 from time import sleep
-from flask import Flask
-from flask import request
-from opentracing.ext import tags
-from opentracing.propagation import Format
-from jaeger_client import Config
+from flask import Flask, request
+from opentelemetry import trace
+from opentelemetry.exporter import jaeger
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 
-# :)
-import random
+jaeger_exporter = jaeger.JaegerSpanExporter(
+    service_name="service-bar",
+    # configure agent
+    agent_host_name="localhost",
+    agent_port=6831,
+)
 
-
-def init_tracer(service):
-    config = Config(
-        config={"sampler": {"type": "const", "param": 1,}, "logging": True,},
-        service_name=service,
-    )
-
-    # this call also sets opentracing.tracer
-    return config.initialize_tracer()
-
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    BatchExportSpanProcessor(jaeger_exporter)
+)
 
 app = Flask(__name__)
-tracer = init_tracer("service-bar")
 
-
-def expensive_calculation(n):
-    if n == 0:
-        return 0
-    elif n == 1 or n == 2:
-        return 1
-    else:
-        return expensive_calculation(n - 1) + expensive_calculation(n - 2)
+FlaskInstrumentor().instrument_app(app)
 
 
 @app.route("/bar")
 def bar():
-    span_ctx = tracer.extract(Format.HTTP_HEADERS, request.headers)
-    span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
-    with tracer.start_active_span("bar", child_of=span_ctx, tags=span_tags) as span:
-        with tracer.start_span("expensive-calculation", child_of=span.span):
-            expensive_calculation(random.randint(1, 10))
-        return "bar"
+    return "bar"
 
 
 if __name__ == "__main__":
